62b13157b72af032f583db084f5f30f41acc4279
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2020-05-04 14:13:23 +0100

    trying to get OTA working

--- src/MQTT.cpp
+++ src/MQTT.cpp
@@ -11,26 +11,6 @@ void MQTT_begin()
 
     setupMQTTClient();
 
-    //set up mDNS name
-    char *hostname = MDNS_HOSTNAME;
-
-    if (!MDNS.begin(hostname))
-    {
-        Serial.println("Error setting up MDNS responder!");
-
-        state.warning("Error setting up MDNS responder!");
-    }
-    else
-    {
-        //set default instance
-        MDNS.setInstanceName(hostname);
-
-        // Add service to MDNS-SD
-        MDNS.addService("ota", "tcp", 1883);
-
-        state.current("MDNS working");
-    }
-
     state.ipAddress(WiFi.localIP().toString());
 }
 
@@ -122,6 +102,8 @@ void MQTT_loop()
     }
 
     MQTTClient.loop();
+
+    ArduinoOTA.handle();
 }
 
 void setupMQTTClient()
@@ -177,30 +159,36 @@ void setupOTA()
     ArduinoOTA.setHostname(MDNS_HOSTNAME);
     ArduinoOTA.setPassword(OTA_PASSWORD);
 
-    ArduinoOTA.onStart([]() {
-        Serial.println("Start");
-    });
+    ArduinoOTA
+        .onStart([]() {
+            String type;
+            if (ArduinoOTA.getCommand() == U_FLASH)
+                type = "sketch";
+            else // U_SPIFFS
+                type = "filesystem";
 
-    ArduinoOTA.onEnd([]() {
-        Serial.println("\nEnd");
-    });
+            // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+            Serial.println("Start updating " + type);
+        })
+        .onEnd([]() {
+            Serial.println("\nEnd");
+        })
+        .onProgress([](unsigned int progress, unsigned int total) {
+            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        })
+        .onError([](ota_error_t error) {
+            Serial.printf("Error[%u]: ", error);
+            if (error == OTA_AUTH_ERROR)
+                Serial.println("Auth Failed");
+            else if (error == OTA_BEGIN_ERROR)
+                Serial.println("Begin Failed");
+            else if (error == OTA_CONNECT_ERROR)
+                Serial.println("Connect Failed");
+            else if (error == OTA_RECEIVE_ERROR)
+                Serial.println("Receive Failed");
+            else if (error == OTA_END_ERROR)
+                Serial.println("End Failed");
+        });
 
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    });
-
-    ArduinoOTA.onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR)
-            Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR)
-            Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR)
-            Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR)
-            Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR)
-            Serial.println("End Failed");
-    });
     ArduinoOTA.begin();
 }
